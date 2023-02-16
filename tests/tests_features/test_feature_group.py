cfa125143dc5a129c1e4e2124cf06b16cfdaf745
Heet Sankesara <heetsankesara3@gmail.com>
2023-02-16 17:40:11 +0530

    added feature group tests

---
+++ tests/tests_features/test_feature_group.py
@@ -0,0 +1,37 @@
+import unittest
+from radarpipeline.features import FeatureGroup, Feature
+
+class TestFeature(unittest.TestCase):
+    class MockFeature(Feature):
+        def __init__(self):
+            self.name = "Mock Feature"
+            self.description = "Mock Feature Description"
+            self.required_input_data = ["mock_data_1"]
+
+        def preprocess(self, data):
+            pass
+
+        def calculate(self, data):
+            pass
+
+    class MockFeature2(Feature):
+        def __init__(self):
+            self.name = "Mock Feature 2"
+            self.description = "Mock Feature Description 2"
+            self.required_input_data = ["mock_data_2"]
+
+        def preprocess(self, data):
+            pass
+
+        def calculate(self, data):
+            pass
+
+    class MockFeatureGroup(FeatureGroup):
+        def preprocess(self, data):
+            pass
+
+    def setUp(self):
+        self.mockfeaturegroup = self.MockFeatureGroup(name='mock feature group', description='mock feature group description', features=[self.MockFeature, self.MockFeature2])
+
+    def test_get_required_data(self):
+        self.assertCountEqual(self.mockfeaturegroup.get_required_data(), ['mock_data_1', 'mock_data_2'])
