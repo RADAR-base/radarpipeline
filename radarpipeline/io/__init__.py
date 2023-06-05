dfee257ce26296daa5a3eabf09478e36c00aca41
Heet Sankesara <heetsankesara3@gmail.com>
2023-06-05 10:23:11 +0100

    Added sftp data reader

--- radarpipeline/io/__init__.py
+++ radarpipeline/io/__init__.py
@@ -1,4 +1,4 @@
 from radarpipeline.io.abc import DataReader, SchemaReader
 from radarpipeline.io.reader import AvroSchemaReader, SparkCSVDataReader, Reader
-from radarpipeline.io.reader import SftpDataReader
+from radarpipeline.io.downloader import SftpDataReader
 from radarpipeline.io.writer import *
