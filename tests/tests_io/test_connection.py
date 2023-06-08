d323a072e24438ed70ac8b7ceb097a7250041298
Heet Sankesara <heetsankesara3@gmail.com>
2023-06-08 21:38:34 +0100

    Updated connection tests

--- tests/tests_io/test_connection.py
+++ tests/tests_io/test_connection.py
@@ -16,8 +16,6 @@ from tempfile import mkdtemp
 STARS8192 = '*' * 8192
 TEST_PATH_DIR = "tests/resources/sftp_test"
 
-# Testing SFTP connector
-
 CONTENT_OBJ = {
     'home': {
         'test': {
@@ -57,156 +55,170 @@ def sftpclient(sftpserver):
     transport.close()
 
 
-@pytest.yield_fixture
-def content(sftpserver):
-    with sftpserver.serve_content(deepcopy(CONTENT_OBJ)):
-        yield
+class TestConnection(unittest.TestCase):
 
+    DIR_LIST = [('pub', ),
+                ('pub', 'foo1'),
+                ('pub', 'foo2'),
+                ('pub', 'foo2', 'bar1')]
+    FILE_LIST = [('pub', 'read.me'),
+                 ('pub', 'make.txt'),
+                 ('pub', 'foo1', 'foo1.txt'),
+                 ('pub', 'foo2', 'foo2.txt'),
+                 ('pub', 'foo2', 'bar1', 'bar1.txt')]
+    TEST_PATH_DIR = "tests/resources/sftp_test"
+    STARS8192 = '*' * 8192
 
-@pytest.mark.xfail(sys.version_info < (2, 7), reason="Intermittently broken on 2.6")
-def test_sftpserver_bound(sftpserver):
-    assert sftpserver.wait_for_bind(1)
+    @pytest.fixture(autouse=True)
+    def prepare_fixture(self, sftpserver):
+        self.sftpserver = sftpserver
 
+    @staticmethod
+    def build_dir_struct(cls, local_path):
+        '''build directory structure'''
+        for dparts in cls.DIR_LIST:
+            os.mkdir(os.path.join(local_path, *dparts))
+        for fparts in cls.FILE_LIST:
+            with open(os.path.join(local_path, *fparts), 'wb') as fhndl:
+                try:
+                    fhndl.write(STARS8192)
+                except TypeError:
+                    fhndl.write(bytes(STARS8192, 'UTF-8'))
 
-def test_sftpserver_available(sftpserver):
-    assert isinstance(sftpserver, SFTPServer)
-    assert sftpserver.is_alive()
-    print(sftpserver)
-    assert str(sftpserver.port) in sftpserver.url
+    @staticmethod
+    def remove_dir_struct(cls, local_path):
+        '''clean up directory struct'''
+        for fparts in cls.FILE_LIST:
+            os.remove(os.path.join(local_path, *fparts))
+        for dparts in reversed(cls.DIR_LIST):
+            os.rmdir(os.path.join(local_path, *dparts))
 
+    @classmethod
+    def setUpClass(cls):
+        cls.build_dir_struct(cls, TEST_PATH_DIR)
 
-def test_connection_with(sftpserver):
-    '''connect to a public sftp server'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        sftp.close()
+    @classmethod
+    def tearDownClass(cls):
+        cls.remove_dir_struct(cls, TEST_PATH_DIR)
 
+    def setUp(self):
+        self.sftp = SftpConnector(conn(self.sftpserver), ['x'])
+        self.home = "/home/test"
 
-def test_cd_path(sftpserver):
-    '''test .cd with a path'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        home = sftp.pwd
-        with sftp.cd('/home/test/pub'):
-            assert sftp.pwd == '/home/test/pub'
-        assert home == sftp.pwd
-        sftp.close()
+    def tearDown(self):
+        self.sftp.close()
 
+    def test_connection_with(self):
+        '''connect to a public self.sftp server'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
 
-def test_cd_nested(sftpserver):
-    '''test nested cd's'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        home = sftp.pwd
-        with sftp.cd('/home'):
-            assert sftp.pwd == '/home'
-            with sftp.cd('test/pub'):
-                assert sftp.pwd == '/home/test/pub'
-            assert sftp.pwd == '/home'
-        assert home == sftp.pwd
-        sftp.close()
+    def test_cd_path(self):
+        '''test .cd with a path'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            home = self.sftp.pwd
+            with self.sftp.cd(f'{self.home}/pub'):
+                assert self.sftp.pwd == f'{self.home}/pub'
+            assert home == self.sftp.pwd
 
+    def test_cd_nested(self):
+        '''test nested cd's'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            home = self.sftp.pwd
+            with self.sftp.cd('/home'):
+                assert self.sftp.pwd == '/home'
+                with self.sftp.cd('test/pub'):
+                    assert self.sftp.pwd == f'{self.home}/pub'
+                assert self.sftp.pwd == '/home'
+            assert home == self.sftp.pwd
 
-def test_listdir(sftpserver):
-    '''test listdir'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub'):
-            print(sftp.pwd)
-            assert sftp.listdir() == ['foo1', 'foo2', 'make.txt']
-        sftp.close()
+    def test_listdir(self):
+        '''test listdir'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub'):
+                assert self.sftp.listdir() == ['foo1', 'foo2', 'make.txt']
+            self.sftp.close()
 
+    def test_listdir_attr(self):
+        '''test listdir'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub'):
+                attrs = self.sftp._sftp.listdir_attr()
+                assert len(attrs) == 3
+                # test they are in filename order
+                assert attrs[0].filename == 'make.txt'
+                assert attrs[1].filename == 'foo1'
+                assert attrs[2].filename == 'foo2'
+                # test that longname is there
+                for attr in attrs:
+                    assert attr.longname is not None
+            self.sftp.close()
 
-def test_listdir_attr(sftpserver):
-    '''test listdir'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub'):
-            attrs = sftp._sftp.listdir_attr()
-            assert len(attrs) == 3
-            # test they are in filename order
-            assert attrs[0].filename == 'make.txt'
-            assert attrs[1].filename == 'foo1'
-            assert attrs[2].filename == 'foo2'
-            # test that longname is there
-            for attr in attrs:
-                assert attr.longname is not None
-        sftp.close()
+    def test_isfile(self):
 
+        '''test .isfile() functionality'''
 
-def test_isfile(sftpserver):
-    '''test .isfile() functionality'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        rfile = '/home/test/pub/make.txt'
-        rdir = 'pub'
-        assert sftp.isfile(rfile)
-        assert sftp.isfile(rdir) is False
-        sftp.close()
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            rfile = f'{self.home}/pub/make.txt'
+            rdir = 'pub'
+            assert self.sftp.isfile(rfile)
+            assert self.sftp.isfile(rdir) is False
+            self.sftp.close()
 
+    def test_get(self):
+        '''download a file'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub/foo1'):
+                with tempfile_containing('') as fname:
+                    self.sftp.get('foo1.txt', fname)
+                    assert open(fname, 'rb').read() == b'content of foo1.txt'
+            self.sftp.close()
 
-def test_get(sftpserver):
-    '''download a file'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub/foo1'):
-            with tempfile_containing('') as fname:
-                sftp.get('foo1.txt', fname)
-                assert open(fname, 'rb').read() == b'content of foo1.txt'
-        sftp.close()
+    def test_get_preserve_mtime(self):
+        '''test that m_time is preserved from local to remote, when get'''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub/foo1'):
+                rfile = 'foo1.txt'
+                with tempfile_containing('') as localfile:
+                    r_stat = self.sftp._sftp.stat(rfile)
+                    self.sftp.get(rfile, localfile, preserve_mtime=True)
+                    assert r_stat.st_mtime == os.stat(localfile).st_mtime
+            self.sftp.close()
 
+    def test_get_d(self):
+        '''test the get_d for remotepath is pwd '.' '''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub/'):
+                localpath = mkdtemp()
+                self.sftp.get_d('.', localpath)
+                checks = [(['', ], ['make.txt', ]), ]
+                for pth, fls in checks:
+                    assert sorted(os.listdir(os.path.join(localpath, *pth))) == fls
+                # cleanup local
+                shutil.rmtree(localpath)
+            self.sftp.close()
 
-def test_get_preserve_mtime(sftpserver):
-    '''test that m_time is preserved from local to remote, when get'''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub/foo1'):
-            rfile = 'foo1.txt'
-            with tempfile_containing('') as localfile:
-                r_stat = sftp._sftp.stat(rfile)
-                sftp.get(rfile, localfile, preserve_mtime=True)
-                assert r_stat.st_mtime == os.stat(localfile).st_mtime
-        sftp.close()
+    def test_get_d_pathed(self):
+        '''test the get_d for localpath, starting deeper then pwd '''
+        with self.sftpserver.serve_content(CONTENT_OBJ):
+            self.sftp.connect()
+            with self.sftp.cd(f'{self.home}/pub/'):
+                localpath = mkdtemp()
+                self.sftp.get_d('foo1', localpath)
 
+                chex = [(['', ],
+                         ['foo1.txt', 'image01.jpg']), ]
+                for pth, fls in chex:
+                    assert sorted(os.listdir(os.path.join(localpath, *pth))) == fls
 
-def test_get_d(sftpserver):
-    '''test the get_d for remotepath is pwd '.' '''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub/'):
-            localpath = mkdtemp()
-            sftp.get_d('.', localpath)
-            checks = [(['', ], ['make.txt', ]), ]
-            for pth, fls in checks:
-                assert sorted(os.listdir(os.path.join(localpath, *pth))) == fls
-            # cleanup local
-            shutil.rmtree(localpath)
-        sftp.close()
-
-
-def test_get_d_pathed(sftpserver):
-    '''test the get_d for localpath, starting deeper then pwd '''
-    with sftpserver.serve_content(CONTENT_OBJ):
-        sftp = SftpConnector(conn(sftpserver), ['x'])
-        sftp.connect()
-        with sftp.cd('/home/test/pub/'):
-            localpath = mkdtemp()
-            sftp.get_d('foo1', localpath)
-
-            chex = [(['', ],
-                     ['foo1.txt', 'image01.jpg']), ]
-            for pth, fls in chex:
-                assert sorted(os.listdir(os.path.join(localpath, *pth))) == fls
-
-            # cleanup local
-            shutil.rmtree(localpath)
-        sftp.close()
+                # cleanup local
+                shutil.rmtree(localpath)
+            self.sftp.close()
