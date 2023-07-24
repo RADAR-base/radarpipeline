f949107370e3e664ab21f41f997cd8bac082999a
Heet Sankesara <heetsankesara3@gmail.com>
2023-07-24 12:46:40 +0100

    Adding pip requirements

--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from setuptools import find_packages, setup
 
 setup(
     name="radarpipeline",
-    version="0.0.1",
+    version="2.0.1",
+    license='Apache',
     description="A python feature generation and visualization package use with RADAR project data.",
     url="https://github.com/RADAR-base/radarpipeline",
     author="Heet Sankesara",
     author_email="heet.sankesara@kcl.ac.uk",
+    download_url='https://github.com/RADAR-base/radarpipeline/archive/refs/tags/v2.0.1.tar.gz',
+    keywords=['mhealth', 'pipeline', 'big-data'],
     packages=find_packages(),
     install_requires=[],
     test_suite="tests",
@@ -16,5 +19,10 @@ setup(
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Science/Research",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 )
