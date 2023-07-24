ad79f007475db1d9c698014a8794aa1c1f590e7d
Heet Sankesara <heetsankesara3@gmail.com>
2023-07-24 15:20:57 +0100

    Updated download_url

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     license='Apache',
     description="A python feature generation and visualization package use with RADAR project data.",
     url="https://github.com/RADAR-base/radarpipeline",
-    download_url='https://github.com/RADAR-base/radarpipeline/archive/refs/tags/v2.0.1.tar.gz',
+    download_url='https://github.com/RADAR-base/radarpipeline/archive/refs/tags/v2.0.1a.tar.gz',
     readme="README.md",
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
